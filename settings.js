aaac574f808c3891b9c2c1bbf194c4f6079712cc
Quwili <96943292+Quwili@users.noreply.github.com>
2022-08-01 01:13:06 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,15 +1,3 @@
-//_______________________ ┏  Info  ┓ _______________________\\
-//
-//   Credit : AlipBot
-//   
-//   Note 
-//   Jangan Jual SC ini ,
-//   Jangan Buang Text ini,
-//   Siapa Mahu Upload Jangan Lupa Credit :),
-//   Siapa Tidak Letak Credit Akan Ambil Tindakan
-//   
-//_______________________ ┏ Make By AlipBot ┓ _______________________\\
-
 
 
 
@@ -17,7 +5,7 @@
 //_______________________ ┏  Setting  ┓ _______________________\\
 
 // Name Creator
-creator = 'Dzakijo'
+creator = 'Aine'
 
 //_______________________ ┏  Apikey  ┓ _______________________\\
 
